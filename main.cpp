4825df0e59d0061bd1d19dcd397742b77183c483
Xenofon Papadopoulos <x.papadopoulos@Kenotom.local>
2021-09-17 16:50:46 +0300

    second commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 900, 900, "Chess", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+        // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
